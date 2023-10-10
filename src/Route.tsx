284bb4a39c29c1ae6398ea95f8dd36ce3d0f93e5
madeval <alvarez.fing@gmail.com>
2023-10-10 17:09:42 -0300

    feat: :sparkles: add new route component

---
+++ src/Route.tsx
@@ -0,0 +1,6 @@
+import { type FC } from 'react'
+import { type IRoute as RouteProps } from './types'
+
+export const Route: FC<RouteProps> = () => {
+  return null
+}
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,23 +1,62 @@
-import { useState, type ReactNode, useEffect } from 'react'
+import { useState, type ReactNode, useEffect, Children } from 'react'
 import { match } from 'path-to-regexp'
 import { EVENT_TYPE } from './consts'
-
-interface PropsComponent {
-  params: Record<string, string>
-  [key: string]: unknown
-}
-
-export interface Route {
-  path: string
-  component: ({ params, ...args }: PropsComponent) => ReactNode
-}
+import { type IRoute } from './types'
 
 interface Props {
-  routes: Route[]
+  routes?: IRoute[]
   defaultPage?: () => ReactNode
+  children?: ReactNode
 }
 
-export const Router = ({ routes, defaultPage = DefaultPage }: Props) => {
+export const Router = ({
+  routes,
+  defaultPage = DefaultPage,
+  children,
+}: Props) => {
+  const { path: currentPath } = usePath()
+
+  const childrenRouteParser = getRouteFromChildren(children)
+  const allRoutes = routes?.concat(childrenRouteParser)
+
+  let paramsUrl = {}
+
+  const PageToRender =
+    allRoutes?.find(({ path }) => {
+      if (path === currentPath) return true
+
+      const matcherUrl = match(path, {
+        decode: decodeURIComponent,
+      })
+      const matched = matcherUrl(currentPath)
+      if (matched === false) return false
+
+      paramsUrl = matched.params
+      return true
+    })?.component ?? defaultPage
+
+  return <PageToRender params={paramsUrl} />
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getRouteFromChildren = (children: any) => {
+  if (typeof children === 'undefined') return
+
+  return Children.map(children, ({ props, type }) => {
+    const { name } = type
+    const { path, component } = props
+
+    const isRoute = name === 'Route'
+    if (!isRoute) return
+
+    return {
+      path,
+      component,
+    }
+  })
+}
+
+const usePath = () => {
   const [currentPath, setCurrentPath] = useState<string>(
     window.location.pathname,
   )
@@ -36,23 +75,9 @@ export const Router = ({ routes, defaultPage = DefaultPage }: Props) => {
     }
   }, [])
 
-  let paramsUrl = {}
-
-  const PageToRender =
-    routes.find(({ path }) => {
-      if (path === currentPath) return true
-
-      const matcherUrl = match(path, {
-        decode: decodeURIComponent,
-      })
-      const matched = matcherUrl(currentPath)
-      if (matched === false) return false
-
-      paramsUrl = matched.params
-      return true
-    })?.component ?? defaultPage
-
-  return <PageToRender params={paramsUrl} />
+  return {
+    path: currentPath,
+  }
 }
 
 const DefaultPage = () => <h1>404</h1>
